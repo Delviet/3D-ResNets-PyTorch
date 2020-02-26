e8d4137bdeaee02660c44fe3619cf01da76d6e48
Delviet <59310708+Delviet@users.noreply.github.com>
2020-02-26 10:31:22 +0300

    Add files via upload

--- utils_/gta_jpg.py
+++ utils_/gta_jpg.py
@@ -31,7 +31,7 @@ def dataset_jpg(dataset_path, jpg_path):
                 dest_path = os.path.join(jpg_path, curr_class, curr_scene, video_name)
                 os.mkdir(dest_path)
                 print(dest_path, 'created')
-                cmd = 'ffmpeg -i {} -vf scale=-1:360 {}/image_%05d.jpg'.format(video, dest_path)
+                cmd = 'ffmpeg -i "{}" -vf scale=-1:360 {}/image_%05d.jpg'.format(video, dest_path)
                 print(cmd)
                 subprocess.call(cmd, shell=True)
                 print('\n')
